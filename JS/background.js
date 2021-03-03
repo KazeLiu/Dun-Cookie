4d1fe060571c82a2e51815053ad9e695682bfcd4
KAZE <kaze.liu@qq.com>
2021-03-03 11:38:25 +0800

    添加各种分类

--- JS/background.js
+++ JS/background.js
@@ -1,6 +1,7 @@
 
 // = 'https://space.bilibili.com/161775300/dynamic'
 // = 'https://space.bilibili.com/456664753/dynamic' 测试 央视新闻
+// 297326 自己
 var Kaze = {
     cardList: [],
     historynew: {},
@@ -18,47 +19,61 @@ var Kaze = {
                 if (info.code == 0 && info.data != null && info.data.cards != null && info.data.cards.length > 0) {
                     info.data.cards.map(x => {
                         let dynamicInfo = JSON.parse(x.card);
-                        that.cardList.push({ card: x.card, time: x.desc.timestamp, dynamicInfo: dynamicInfo, type: dynamicInfo["item"] == undefined ? 0 : 1 })
+                        let type = that.GetdynamicType(dynamicInfo);
+                        let origindynamicInfo = {};
+                        let origindyType = -1;
+                        if (type == 2) {
+                            origindynamicInfo = JSON.parse(dynamicInfo.origin);
+                            origindyType = that.GetdynamicType(origindynamicInfo);
+                        }
+                        that.cardList.push({
+                            card: x.card, time: x.desc.timestamp, dynamicInfo: dynamicInfo, type: type,
+                            origindynamicInfo: origindynamicInfo,
+                            origindyType: origindyType
+                        })
                     });
                     that.apinew = that.cardList[0];
                     chrome.storage.local.get(['cardList'], result => {
                         if (result != null && result.cardList != null && result.cardList.length > 0) {
                             that.historynew = result.cardList[0];
                             if (that.historynew.time != that.apinew.time) {
-                                //console.log(新动态)
                                 let dynamicInfo = that.apinew.dynamicInfo;
-                                // type==0 视频 ==1 动态
-                                if (that.apinew.type == 0) {
-                                    chrome.notifications.create(that.historynew.time + '', {
-                                        iconUrl: '../image/icon.png',
-                                        message: dynamicInfo.dynamic,
-                                        title: `饼来了!${dynamicInfo.title}`,
-                                        imageUrl: dynamicInfo.pic,
-                                        type: "image"
-                                    });
+                                let type = that.apinew.type;
+                                let origindynamic = that.apinew.origindynamicInfo;
+                                let origindyType = that.apinew.origindyType;
+                                //参数反复利用
+                                dynamicInfo = dynamicInfo.item;
+                                // type==0 视频 ==1 动态 ==2转发
+                                if (type == 0) {
+                                    that.SendNotice("image", `饼来了!${dynamicInfo.title}`, dynamicInfo.dynamic, dynamicInfo.pic)
                                 } else {
-                                    dynamicInfo = dynamicInfo.item;
-                                    if (dynamicInfo.pictures != null && dynamicInfo.pictures.length > 0) {
-                                        chrome.notifications.create(that.historynew.time + '', {
-                                            iconUrl: '../image/icon.png',
-                                            message: dynamicInfo.description,
-                                            title: `饼来了!`,
-                                            imageUrl: dynamicInfo.pictures[0].img_src,
-                                            type: "image"
-                                        });
+                                    if (type == 2) {
+                                        //转发数据
+                                        if (origindyType == 1) {
+                                            dynamicInfo.content = `${dynamicInfo.content}//@${origindynamic?.user?.name}:${origindynamic?.item?.description}`;
+                                            if (origindynamic?.item?.pictures != undefined) {
+                                                that.SendNotice("image", `[转发动态]饼来了!`, `${dynamicInfo.content}//@${origindynamic.user.name}:${origindynamic.item.description}`, origindynamic.item.pictures[0].img_src)
+                                            } else {
+                                                that.SendNotice("basic", `[转发动态]饼来了!`, `${dynamicInfo.content}//@${origindynamic.user.name}:${origindynamic.item.description}`);
+                                            }
+                                        } else if (origindyType == 2) {
+                                            that.SendNotice("basic", `[转发内容]饼来了!`, `套娃转发 不解析了`);
+                                        } else if (origindyType == 0) {
+                                            that.SendNotice("image", `[转发视频]饼来了!${origindynamic.title}`, `${dynamicInfo.content}//@${origindynamic.owner.name}:${origindynamic.title}`, origindynamic.pic);
+                                        }
                                     } else {
-                                        chrome.notifications.create(that.historynew.time + '', {
-                                            iconUrl: '../image/icon.png',
-                                            message: dynamicInfo.description,
-                                            title: `饼来了!`,
-                                            type: "basic"
-                                        });
+                                        if (dynamicInfo?.pictures != undefined) {
+                                            that.SendNotice("image", `饼来了!`, dynamicInfo?.content || dynamicInfo?.description, dynamicInfo?.pictures[0].img_src)
+                                        } else {
+                                            that.SendNotice("basic", `饼来了!`, dynamicInfo?.content || dynamicInfo?.description);
+                                        }
                                     }
+
 
                                 }
 
                                 chrome.storage.local.set({ cardList: that.cardList });
-                            } 
+                            }
                         } else {
                             chrome.storage.local.set({ cardList: that.cardList });
                         }
@@ -82,6 +97,37 @@ var Kaze = {
             }, result.time);
         });
 
+    },
+    SendNotice(type, title, message, imageUrl) {
+        if (imageUrl) {
+            chrome.notifications.create(this.historynew.time + '', {
+                iconUrl: '../image/icon.png',
+                message: message,
+                title: title,
+                imageUrl: imageUrl,
+                type: type
+            });
+        } else {
+            chrome.notifications.create(this.historynew.time + '', {
+                iconUrl: '../image/icon.png',
+                message: message,
+                title: title,
+                type: type
+            });
+        }
+
+    },
+    GetdynamicType(dynamicInfo) {
+        let type = -1;
+        if (dynamicInfo["item"] == undefined) {
+            type = 0;
+        }
+        else if (dynamicInfo["item"] != undefined && dynamicInfo["origin"] != undefined) {
+            type = 2;
+        } else if (dynamicInfo["item"] != undefined) {
+            type = 1;
+        }
+        return type;
     },
     Init() {
         this.Getdynamic();
